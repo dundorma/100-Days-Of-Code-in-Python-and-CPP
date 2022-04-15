2be7d5bca8ab74aeb43f0b5063b6a025417390d0
Rizky Arif <90293691+dundorma@users.noreply.github.com>
2022-04-15 14:29:02 +0700

    Create exercise-1.cpp

---
+++ Day-1/exercise-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+  std::cout << "Day 1 - Python Print Function" << std::endl;
+  std::cout << "The function is declared like this:" << std::endl;
+  std::cout << "print('what to print')" << std::endl;
+  
+  return 0;
+}
