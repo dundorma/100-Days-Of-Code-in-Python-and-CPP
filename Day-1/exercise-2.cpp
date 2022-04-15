240482234975b7c7511de86a83d114a148f999ea
Rizky Arif <90293691+dundorma@users.noreply.github.com>
2022-04-15 14:35:55 +0700

    Create exercise-2.cpp

---
+++ Day-1/exercise-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Day 1 - String Manipulation" << std::endl;
+    std::cout << "String Concatenation is done with the \"+\"  sign." << std::endl;
+    std::cout << "e.g. print(\"Hello \" + \"world\")" << std::endl;
+    std::cout << "New lines can be created with a backslash and n." << std::endl;
+
+    return 0;
+}
